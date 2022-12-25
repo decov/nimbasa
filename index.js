906a7377e67270d008d55dd9573262c52645d662
furstamp <gyket.pentest.133971@protonmail.com>
2022-12-25 17:17:53 -0300

    adições de alguns módulos da package discord.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const { Client } = require('discord.js');
-const client = new Client({ intents: ["Guilds"] });
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const client = new Client({
+    intents: [Guilds, GuildMembers, GuildMessages],
+    partials: [User, Message, GuildMember, ThreadMember]
+});
 
 client.config = require('./config.json');
+
+client.events = new Collection();
 
 client
     .login(client.config.TOKEN)
