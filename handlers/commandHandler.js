47976c3d801a31994d2688e02d78f96e991dafe7
furstamp <gyket.pentest.133971@protonmail.com>
2022-12-26 01:20:27 -0300

    adição da etapa inicial da tabela para comandos

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,3 +1,7 @@
 async function loadCommands(client) {
     const { loadFiles } = require('../functions/fileLoader');
+
+    const ascii = require('ascii-table');
+
+    const table = new ascii().setHeading('comandos', 'status');
 }
