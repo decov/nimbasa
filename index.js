f8e924fd5bc461bf0d02cbd492803015b351590b
furstamp <gyket.pentest.133971@protonmail.com>
2022-12-26 00:49:44 -0300

    adição de alguns comentários no arquivo index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// consts
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember } = Partials;
@@ -7,15 +8,16 @@ const client = new Client({
     partials: [User, Message, GuildMember, ThreadMember]
 });
 
+// setando o arquivo que contém configs
 client.config = require('./config.json');
 
 client.events = new Collection();
 
 client
-    .login(client.config.TOKEN)
+    .login(client.config.TOKEN) // logando o client com o token no arquivo config.json
     .then(() => {
-        console.log(`client logado como ${client.user.username}...`);
+        console.log(`client logado como ${client.user.username}...`); // log no terminal
         
-        client.user.setActivity(`em ${client.guilds.cache.size} servidores`);
+        client.user.setActivity(`em ${client.guilds.cache.size} servidores`); // status do bot
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err)); // log de erro, caso aconteça
