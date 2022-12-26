d32b6bd5e3ac63e7918bf25fd89594086957995d
furstamp <gyket.pentest.133971@protonmail.com>
2022-12-26 01:09:22 -0300

    primeira da etapa de lidar com eventos finalizada

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,3 +1,34 @@
 async function loadEvents(client) {
     const { loadFiles } = require('../functions/fileLoader');
+
+    const ascii = require('ascii-table');
+    const table = new ascii().setHeading('eventos', 'status');
+
+    await client.events.clear();
+
+    const files = await loadFiles('events');
+
+    files.forEach((file) => {
+        const event = require(file);
+
+        const execute = (...args) => event.execute(...args, client);
+
+        client.events.set(event.name, execute);
+
+        if (event.rest) {
+            if (event.once)
+                client.rest.once(event.name, execute);
+            else
+                client.rest.on(event.name, execute);
+        } else {
+            if (event.once)
+                client.once(event.name, execute);
+            else
+                client.on(event.name, execute);
+        }
+
+        table.addRow(event.name, '✔️');
+    })
+
+    return console.log(table.toString(), "\neventos inicializados com sucesso...");
 }
