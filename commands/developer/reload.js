1cd9da5645afdb7ea3514495602f0155968a285b
furstamp <gyket.pentest.133971@protonmail.com>
2022-12-30 04:32:06 -0300

    comando para recarregar comandos e eventos

---
+++ commands/developer/reload.js
@@ -0,0 +1,67 @@
+// discord.js
+const {
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    Client
+} = require("discord.js");
+
+// constando os handlers de comandos e eventos
+const { loadCommands } = require('../../handlers/commandHandler');
+const { loadEvents } = require('../../handlers/eventHandler');
+
+module.exports = {
+    developer: true,
+
+    data: new SlashCommandBuilder()
+        .setName('reload')
+        .setDescription('recarrega todos os comandos e eventos do bot...')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+
+        .addSubcommand((options) => options
+            .setName('eventos')
+            .setDescription('recarrega os eventos do bot...'))
+            
+        .addSubcommand((options) => options
+            .setName('comandos')
+            .setDescription('recarrega os comandos do bot...')
+        ),
+
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+
+    execute(interaction, client) {
+        const subCommand = interaction.options.getSubcommand();
+
+        switch(subCommand) {
+            case "eventos" : {
+                for (const [key, value] of client.events)
+
+                client.removeListener(`${key}`, value, true);
+
+                loadEvents(client);
+
+                interaction.reply({
+                    content: "eventos recarregados...",
+                    ephemeral: true
+                });
+            }
+
+            break;
+
+            case "comandos" : {
+                loadCommands(client);
+
+                interaction.reply({
+                    content: "comandos recarregados...",
+                    ephemeral: true
+                });
+            }
+
+            break;
+        }
+    }
+}
